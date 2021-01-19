b84a96734f5ac40bbb6cdf61e6e6453c72939b58
Kash <kashay@agyeman.me>
2021-01-19 07:23:49 -0700

    connected to 5-day forecast api

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -7,18 +7,24 @@ $(document).ready(function () {
   // Function to connect to today's current weather
   function currentWeather(today){
 
-    let currentApiUrl = 'api.openweathermap.org/data/2.5/weather?q=' + 'phoenix' + '&appid=' + apiKey
+    let currentApiUrl = 'api.openweathermap.org/data/2.5/weather?q=' + 'phoenix' + '&appid=' + apiKey;
     console.log(currentApiUrl);
-    console.log('Connected to Current Weather API')
+    console.log('Connected to Current Weather API');
 
   };
 
 
-  // 
+  // Function to connect to 5-day forecast's API
+  function fiveDayForecast(forcast){
+
+    let forecastApiUrl = 'api.openweathermap.org/data/2.5/forecast?q=' + 'phoenix' + '&appid=' + apiKey;
+    console.log(forecastApiUrl)
+    console.log('Connected to 5-Day Forecast API')
+  }
 
 
   //Function Hardcoded Calls
-
+  fiveDayForecast();
   currentWeather();
 
 
