6875b80450fec0c0e57ff5f4594737f0dc80afcb
Kash <kashay@agyeman.me>
2021-01-19 07:18:39 -0700

    connect to current weather api

---
+++ assets/js/javascript.js
@@ -0,0 +1,56 @@
+$(document).ready(function () {
+
+  // GOLOBAL VARIABLES //
+  const apiKey = '9f9176e2a3294a9e5a94e22016800ff4';
+
+
+  // Function to connect to today's current weather
+  function currentWeather(today){
+
+    let currentApiUrl = 'api.openweathermap.org/data/2.5/weather?q=' + 'phoenix' + '&appid=' + apiKey
+    console.log(currentApiUrl);
+    console.log('Connected to Current Weather API')
+
+  };
+
+
+  // 
+
+
+  //Function Hardcoded Calls
+
+  currentWeather();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
