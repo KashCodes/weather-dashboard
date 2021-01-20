d571093dfa703dabb8b7ecd5712da5597896ab58
Kash <kashay@agyeman.me>
2021-01-19 20:57:03 -0700

    fetch added to API data needed

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -8,10 +8,18 @@ $(document).ready(function () {
   function currentWeather(today){
 
     /* Need to set up function to capture city and replace with variable in API   */
-    let currentApiUrl = 'api.openweathermap.org/data/2.5/weather?q=' + 'phoenix' + '&appid=' + apiKey;
-    console.log(currentApiUrl);
-    console.log('Connected to Current Weather API');
+    let currentApiUrl = 'http://api.openweathermap.org/data/2.5/weather?q=' + 'phoenix' + '&appid=' + apiKey;
 
+    // fetch currentWeather
+    fetch(currentApiUrl).then(function(response){
+      response.json().then(function(currentData){
+        console.log(currentData);
+        console.log('JSON currentData');
+      });
+      // console.log(response);
+    });
+    // console.log(currentApiUrl);
+    console.log('Connected to Current Weather API');
   };
 
 
@@ -19,24 +27,44 @@ $(document).ready(function () {
   function fiveDayForecast(forcast){
 
     /* Need to set up function to capture city and replace with variable in API   */
-    let forecastApiUrl = 'api.openweathermap.org/data/2.5/forecast?q=' + 'phoenix' + '&appid=' + apiKey;
-    console.log(forecastApiUrl)
+    let forecastApiUrl = 'http://api.openweathermap.org/data/2.5/forecast?q=' + 'phoenix' + '&appid=' + apiKey;
+
+    // fetch forecastApiUrl 
+    fetch(forecastApiUrl).then(function(response){
+      response.json().then(function(forecastData){
+        console.log(forecastData);
+        console.log('JSON forecastData')
+      })
+      // console.log(response)
+    });
+    // console.log(forecastApiUrl)
     console.log('Connected to 5-Day Forecast API')
   };
 
   //Function to connect to UV Index's API
   function uvIndex(index){
-    let uvIndexApiUrl = 'http://api.openweathermap.org/data/2.5/uvi?lat=' + {lat} '&lon=' + {lon} '&appid=' + apiKey;
-    console.log(uvIndex);
+    let uvIndexApiUrl = 'http://api.openweathermap.org/data/2.5/uvi?lat=' + '37.75' + '&lon=' + '122.37'+ '&appid=' + apiKey;
+    
+    //fetch uvIndex
+    fetch(uvIndexApiUrl).then(function(response){
+      response.json().then(function(uvData){
+        console.log(uvData);
+        console.log('JSON uvData')
+      });
+      // console.log(response);
+    });
+    // console.log(uvIndexApiUrl);
     console.log('Connected to UV Index API')
   };
 
   //Add Event Listen for Button click and pull search data
 
   //Function Hardcoded Calls
-  uvIndex();
-  fiveDayForecast();
   currentWeather();
+  fiveDayForecast();
+  uvIndex();
+  
+
 
 
 
